b55a6bac70743bec166fc3b60870a79de38407ee
xucher <1787355627@qq.com>
2018-05-28 19:01:13 +0800

    Add project files.

---
+++ ExpressTrack/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using ExpressTrack.Models;
+
+namespace ExpressTrack {
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
+	}
+}
---
+++ ExpressTrack/Models.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressTrack.Models {
+	class Express {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public Express(int id, string name) {
+			Id = id;
+			Name = name;
+		}
+	}
+}
