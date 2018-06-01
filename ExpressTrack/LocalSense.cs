952937bef3d3d73bf508eb76b141c874a940a44d
xucher <1787355627@qq.com>
2018-06-01 17:55:38 +0800

    finish UI

---
+++ ExpressTrack/LocalSense.cs
@@ -0,0 +1,61 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using WebSocketSharp;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ExpressTrack.Device {
+//    class LocalSense {
+//        public delegate void ConnectHandle(WebSocketHandle w, EventArgs e);
+//        public event ConnectHandle OnConnect;
+//        public int[] location = null
+//            ;
+//        public int[] connect_server() {
+//            //连接服务器
+//            WebSocket ws = new WebSocket("ws://" + "192.168.0.151" + ":9001", "localSensePush-protocol");
+
+//            int id;
+//            int x;
+//            int y;
+
+//            ws.OnOpen += (sender, e) => MessageBox.Show("已经与服务器建立连接");
+//            ws.OnMessage += (sender, e) => {
+//                string data = e.Data;
+//                string[] tempdata = data.Split('-');
+//                int msgtype;
+
+//                if (tempdata[0] == "CC" && tempdata[1] == "5F" && tempdata[2] == "01") {
+//                    msgtype = 1;//表示标签实时信息
+
+//                    int tagnum = Convert.ToInt16(tempdata[3], 16);
+//                    for (int i = 0; i < tagnum; i++) {
+
+//                        id = Convert.ToInt16(tempdata[4 + 21 * (tagnum - 1)] + tempdata[5 + 21 * (tagnum - 1)], 16);
+//                        x = Convert.ToInt16(tempdata[6 + 21 * (tagnum - 1)] + tempdata[7 + 21 * (tagnum - 1)] + tempdata[8 + 21 * (tagnum - 1)] + tempdata[9 + 21 * (tagnum - 1)], 16);
+//                        y = Convert.ToInt16(tempdata[10 + 21 * (tagnum - 1)] + tempdata[11 + 21 * (tagnum - 1)] + tempdata[12 + 21 * (tagnum - 1)] + tempdata[13 + 21 * (tagnum - 1)], 16);
+//                        if (location == null) {
+//                            location = new int[3];
+//                        }
+//                        location[0] = id;
+//                        location[1] = x;
+//                        location[2] = y;
+
+//                        if (OnConnect != null) {
+//                            OnConnect(this, e);
+//                        }
+//                    }
+//                }
+
+//            };
+//            ws.Connect();
+//            ws.OnClose += (sender, e) => {
+//                Console.WriteLine("已经和服务器断开，关闭状态为：" + e.Code + "关闭原因是：" + e.Reason);
+//            };
+//            ws.OnError += (sender, e) => {
+//                Console.WriteLine("Websocket异常，错误信息为" + e.Message);
+//            };
+//            return location;
+//        }
+//    }
+//}
--- ExpressTrack/Pages/ExpressListPage.xaml.cs
+++ ExpressTrack/Pages/ExpressListPage.xaml.cs
@@ -1,31 +1,21 @@
-﻿using System;
+﻿using ExpressTrack.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using System.Collections.ObjectModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ExpressTrack.Models;
 
 namespace ExpressTrack {
-	public partial class ExpressListPage : Page {
+    public partial class ExpressListPage : Page {
 		private List<Express> expresses = new List<Express>();
 		public ExpressListPage() {
 			InitializeComponent();
-		}
+        }
 
 		private void Page_Initialized(object sender, EventArgs e) {
-			for (int i = 1;i < 10;i++) {
-				expresses.Add(new Express(i, "express" + i));
-			}
-			DG_expressList.ItemsSource = expresses;
-		}
-	}
+            for (int i = 1; i < 10; i++) {
+                expresses.Add(new Express(i, "express" + i));
+            }
+            DG_expressList.ItemsSource = expresses;
+        }
+    }
 }
