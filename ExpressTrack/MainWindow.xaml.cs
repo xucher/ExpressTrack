4aaf5a266af546b165189e3204fa0e34b7e7322b
xucher <1787355627@qq.com>
2018-06-02 10:28:52 +0800

    add login page

--- ExpressTrack/MainWindow.xaml.cs
+++ ExpressTrack/MainWindow.xaml.cs
@@ -10,38 +10,6 @@ namespace ExpressTrack {
 			InitializeComponent();
 		}
 
-		private void TreeView_PreviewMouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e) {
-			NavigationService nav = mainFrame.NavigationService;
-			if (actionTree.SelectedItem != null) {
-				string name = (actionTree.SelectedItem as TreeViewItem).Header.ToString();
-				switch (name) {
-					case "快递列表":
-						nav.Navigate(new Uri("Pages/ExpressListPage.xaml", UriKind.Relative));
-						break;
-					case "快递入库":
-						nav.Navigate(new Uri("Pages/InstockPage.xaml", UriKind.Relative));
-						break;
-					case "快递详情":
-						nav.Navigate(new Uri("Pages/ExpressDetailPage.xaml", UriKind.Relative));
-						break;
-					case "快递出库":
-						nav.Navigate(new Uri("Pages/OutstockPage.xaml", UriKind.Relative));
-						break;
-					case "用户列表":
-						nav.Navigate(new Uri("Pages/UserPage.xaml", UriKind.Relative));
-						break;
-					case "职员列表":
-						nav.Navigate(new Uri("Pages/WorkerPage.xaml", UriKind.Relative));
-						break;
-					case "库存明细":
-						nav.Navigate(new Uri("Pages/StockPage.xaml", UriKind.Relative));
-						break;
-					default: break;
-				}
-			}
-			
-		}
-
 		private void Window_MouseDown(object sender, MouseButtonEventArgs e) {
 			if (e.LeftButton == MouseButtonState.Pressed) {
 				DragMove();
---
+++ ExpressTrack/Pages/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpressTrack.Pages {
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void TreeView_PreviewMouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            NavigationService nav = mainFrame.NavigationService;
+            if (actionTree.SelectedItem != null) {
+                string name = (actionTree.SelectedItem as TreeViewItem).Header.ToString();
+                switch (name) {
+                    case "快递列表":
+                        nav.Navigate(new Uri("Pages/ExpressListPage.xaml", UriKind.Relative));
+                        break;
+                    case "快递入库":
+                        nav.Navigate(new Uri("Pages/InstockPage.xaml", UriKind.Relative));
+                        break;
+                    case "快递详情":
+                        nav.Navigate(new Uri("Pages/ExpressDetailPage.xaml", UriKind.Relative));
+                        break;
+                    case "快递出库":
+                        nav.Navigate(new Uri("Pages/OutstockPage.xaml", UriKind.Relative));
+                        break;
+                    case "用户列表":
+                        nav.Navigate(new Uri("Pages/UserPage.xaml", UriKind.Relative));
+                        break;
+                    case "职员列表":
+                        nav.Navigate(new Uri("Pages/WorkerPage.xaml", UriKind.Relative));
+                        break;
+                    case "库存明细":
+                        nav.Navigate(new Uri("Pages/StockPage.xaml", UriKind.Relative));
+                        break;
+                    default: break;
+                }
+            }
+        }
+    }
+}
